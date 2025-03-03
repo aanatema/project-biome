19da05edfe643de33bba695da23edd1a06f422af
romaneboireau <romane.boireau@abtasty.com>
2025-03-03 15:43:22 +0100

    fetch book ro display on the front (wip)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import type { Books } from "../../back-end/src/books.ts";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [books, setBooks] = useState<Books[]>([]); // const [formData, setFormData] = useEffect({isbn: '', title: '', author:'', review: ''});
+
+  // const postBtn = document.getElementById('post');
+  // const getBtn = document.getElementById('get');
+
+  // const content = document.getElementsByClassName('content');
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/books_list");
+      const data: Books[] = await response.json(); // ✅ Assurer le type des données
+      if (Array.isArray(data)) {
+        setBooks(data.bookList);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la récupération des livres", error);
+    }
+  };
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>isbn</p>
+        <input id="isbn" className="content" />
+
+        <p>title</p>
+        <input id="title" className="content" />
+
+        <p>author</p>
+        <input id="author" className="content" />
+
+        <p>review</p>
+        <textarea id="review" className="content" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div>
+        <button id="post"> POST BOOK </button>
+        <button id="get" onClick={fetchBooks}>
+          {" "}
+          GET BOOK
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* <button id="get" onClick={fetchBooks}> GET REVIEW</button> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {console.log("Book state:", books)}{" "}
+      <ul>
+        {Array.isArray(books) &&
+          books.map((book, index) => (
+            <li key={index}>
+              {book.isbn} - {book.title}
+            </li>
+          ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
