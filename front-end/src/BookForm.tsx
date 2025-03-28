797cbb458678d93999789265cef3a97b58f96e0e
romaneboireau <romane.boireau@abtasty.com>
2025-03-28 16:13:47 +0100

    form is functional

--- front-end/src/BookForm.tsx
+++ front-end/src/BookForm.tsx
@@ -22,21 +22,23 @@ export function BookForm() {
 
   // data is an object with the properties of BookFormProps and the values that will be added through the form
   // async to imitate data being sent to the server
-  const onSubmit: SubmitHandler<BookFormProps> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const onSubmit: SubmitHandler<BookFormProps> = (data) => {
     console.log(data);
   };
 
-  const isbnRegex = /^(97(8|9))?\d{9}(\d|X)$/;
-  const issnRegex = /^ISSN\s?\d{4}-\d{3}[\dX]$/;
+  // NOT WORKING FOR NOW GO BACK LATER
+  // const isbnRegex = /^(97(8|9))?\d{9}(\d|X)$/;
+  // const issnRegex = /^ISSN\s?\d{4}-\d{3}[\dX]$/;
+  // const isbnOrIssnRegex = /^(97(8|9))?\d{9}(\d|X)$|^ISSN\s?\d{4}-\d{3}[\dX]$/;
 
   return (
+  
     <form className="book-form" onSubmit={handleSubmit(onSubmit)}>
       {/* the ... is the spread operator, it takes the properties of 'isbn', passing through 'register' to add them to this input  */}
       <input
         {...register("isbn", {
           required: "ISBN / ISSN may be missing or incorrect",
-          pattern: isbnRegex || issnRegex,
+          // pattern: isbnOrIssnRegex,
         })}
         type="text"
         placeholder="isbn or issn"
