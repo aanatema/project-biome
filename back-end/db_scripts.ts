c419585744ede72643e8ef8063d01e1dc14a3a9f
romaneboireau <romane.boireau@abtasty.com>
2025-04-04 11:36:19 +0200

    comment db scripts

--- back-end/db_scripts.ts
+++ back-end/db_scripts.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { bookList } from "./src/books";
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
 // User
 // async function addUser() {
@@ -23,43 +22,43 @@ const prisma = new PrismaClient();
 //     return prisma.$disconnect().then(() => process.exit(1));
 //   });
 
-async function modifyUser() {}
+// async function modifyUser() {}
 
 // add a new book
-async function addBook() {
-  const newBook = await prisma.media.create({
-    data: {
-      type: "BOOK",
-      book: {
-        create: {
-          isbn: "abcd",
-          title: "book one",
-          author: "author 1",
-        },
-      },
-      reviews: {
-        create: [
-          { 
-            content: "the first review", 
-            rating: 4.5,
-            userId: '3c6ba8f1-9fd8-4733-bd5f-0ba614be73ad',
-          }
-        ],
-      },
-    },
-  });
-  console.log(newBook)
-}
+// async function addBook() {
+//   const newBook = await prisma.media.create({
+//     data: {
+//       type: "BOOK",
+//       book: {
+//         create: {
+//           isbn: "abcd",
+//           title: "book one",
+//           author: "author 1",
+//         },
+//       },
+//       reviews: {
+//         create: [
+//           { 
+//             content: "the first review", 
+//             rating: 4.5,
+//             userId: '3c6ba8f1-9fd8-4733-bd5f-0ba614be73ad',
+//           }
+//         ],
+//       },
+//     },
+//   });
+//   console.log(newBook)
+// }
 
-addBook()
-  .then(() => {
-    console.log("✅ Done!");
-    return prisma.$disconnect();
-  })
-  .catch((e) => {
-    console.error(e);
-    return prisma.$disconnect().then(() => process.exit(1));
-  });
+// addBook()
+//   .then(() => {
+//     console.log("✅ Done!");
+//     return prisma.$disconnect();
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//     return prisma.$disconnect().then(() => process.exit(1));
+//   });
 
 // Review
 // async function addReview() {}
