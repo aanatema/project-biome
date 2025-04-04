705fd8c4a8a0c79356a53e473b49314a56ef9991
romaneboireau <romane.boireau@abtasty.com>
2025-04-04 10:36:05 +0200

    restore some post request (wip)

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,16 +2,74 @@
 
 import express, { type Request, type Response } from "express";
 import { bookList } from "./src/books";
-import cors from 'cors';
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = 3000;
+
 // maybe change this to be more secure ? localhost:3000 doesn't work
-app.use(cors({
- origin: "http://localhost:5173",
-}))
+app.use(
+  express.json(),
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 // model : app.METHOD(PATH,HANDLER)
+
+// POST REQUESTS
+
+app.post("/new_user", async (req: Request, res: Response) => {
+  const { username, email } = req.body;
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+      },
+    });
+    res.status(201).json({ newUser });
+    console.log({ newUser });
+  } catch (error) {
+    console.error("Something happened during the user's creation", error);
+    res
+      .status(500)
+      .json({ error: "Something happened during the user's creation" });
+  }
+});
+
+app.post("/new_media/new_book", async(req:Request, res: Response) => {
+  const { isbn, title, author } = req.body;
+  try {
+    const newBook = await prisma.media.create({
+      data: {
+        type: "BOOK",
+        book: {
+          create: {
+            isbn,
+            title,
+            author,
+          },
+        },
+      },
+    });
+    res.status(201).json({ newBook });
+    console.log(newBook)
+  }catch (error){
+    console.error("Something happened during the creation of a new book", error)
+    res.status(500);
+  }
+})
+
+// add a book
+app.post("/post/book", (req: Request, res: Response) => {
+  const { isbn, title, author, review } = req.body;
+  console.log("book added : ", req.body);
+  res.send("example");
+});
 
 // GET REQUESTS
 app.get("/books_list", (req: Request, res: Response) => {
@@ -62,19 +120,15 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ content: "this is some content", url: req.url });
 });
 
-// POST REQUESTS
-// add a book
-app.post("/post/book", (req: Request, res: Response) => {
-  const {isbn, title, author, review} = req.body; 
-  console.log('book added : ', req.body);
-  res.send("example");
-});
+
 
 // delete the book
-app.delete("/delete/isbn/:isbn/reviews/:reviewId", (req: Request, res: Response) => {
-  
-  res.end();
-});
+app.delete(
+  "/delete/isbn/:isbn/reviews/:reviewId",
+  (req: Request, res: Response) => {
+    res.end();
+  }
+);
 
 app.listen(PORT, () => {
   console.log("Server started and listening on port", PORT);
