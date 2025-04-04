11b102d59e37b38172c3664e151987701812bdfa
romaneboireau <romane.boireau@abtasty.com>
2025-04-04 11:36:25 +0200

    comment db scripts

--- back-end/dist/db_scripts.js
+++ back-end/dist/db_scripts.js
@@ -1,22 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 // User
 // async function addUser() {
 //   const newUser = await prisma.user.create({
 //     data: {
 //       username: "test123",
-//       email: "email@example.com"
+//       email: "second.email@example.com"
 //     }
 //   })
 //   console.log(newUser)
@@ -30,45 +20,40 @@ const prisma = new client_1.PrismaClient();
 //     console.error(e);
 //     return prisma.$disconnect().then(() => process.exit(1));
 //   });
-function modifyUser() {
-    return __awaiter(this, void 0, void 0, function* () { });
-}
+// async function modifyUser() {}
 // add a new book
-function addBook() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const newBook = yield prisma.media.create({
-            data: {
-                type: "BOOK",
-                book: {
-                    create: {
-                        isbn: "abcd",
-                        title: "book one",
-                        author: "author 1",
-                    },
-                },
-                reviews: {
-                    create: [
-                        {
-                            content: "the first review",
-                            rating: 4.5,
-                            userId: 'USER ABCD',
-                            createdAt: ''
-                        }
-                    ],
-                },
-            },
-        });
-        console.log(newBook);
-    });
-}
-addBook()
-    .then(() => {
-    console.log("✅ Done!");
-    return prisma.$disconnect();
-})
-    .catch((e) => {
-    console.error(e);
-    return prisma.$disconnect().then(() => process.exit(1));
-});
+// async function addBook() {
+//   const newBook = await prisma.media.create({
+//     data: {
+//       type: "BOOK",
+//       book: {
+//         create: {
+//           isbn: "abcd",
+//           title: "book one",
+//           author: "author 1",
+//         },
+//       },
+//       reviews: {
+//         create: [
+//           { 
+//             content: "the first review", 
+//             rating: 4.5,
+//             userId: '3c6ba8f1-9fd8-4733-bd5f-0ba614be73ad',
+//           }
+//         ],
+//       },
+//     },
+//   });
+//   console.log(newBook)
+// }
+// addBook()
+//   .then(() => {
+//     console.log("✅ Done!");
+//     return prisma.$disconnect();
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//     return prisma.$disconnect().then(() => process.exit(1));
+//   });
 // Review
 // async function addReview() {}
