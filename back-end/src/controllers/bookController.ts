5aec851b6311478244e33499e841607e4a01ad74
romaneboireau <romane.boireau@abtasty.com>
2025-04-16 11:07:59 +0200

    change for the way we catch errors + update in the messages

--- back-end/src/controllers/bookController.ts
+++ back-end/src/controllers/bookController.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function newBookMedia(req: Request, res: Response){
+export async function newBookMedia(req: Request, res: Response) {
   const { isbn, title, author, reviews } = req.body;
 
   try {
@@ -30,40 +30,56 @@ export async function newBookMedia(req: Request, res: Response){
     res.status(201).json({ newBook });
     console.log("newBook index file", newBook);
   } catch (error) {
-    console.error(
-      "Something happened during the creation of a new book",
-      error
-    );
-    res.status(500);
+    console.error("Error during the creation of a new book", error);
+    res
+      .status(500)
+      .json({ error: "Something happened when creating a new book" });
   }
-};
+}
 
-export async function allBooks(req: Request, res: Response){
-  // retrieve the potentials parameters 
+export async function allBooks(req: Request, res: Response) {
+  // retrieve the potentials parameters
   const { isbn, title, author, reviews } = req.query;
 
-  try{
-    const books = await prisma.book.findMany();
+  try {
+    // searchBy x logic
+    const books = await prisma.book.findMany({
+      where: {
+        isbn: isbn ? String(isbn) : undefined,
+        title: title ? String(title) : undefined,
+        author: author ? String(author) : undefined,
+      },
+    });
     res.status(200).json({ books });
-  }catch (error){
-    // console.error("Something happened when retrieving the books")
-    res.status(500).json({error: "Something happened when retrieving the books" });
-
+  } catch (error) {
+    console.error("Error while retrieving the books", error);
+    res
+      .status(500)
+      .json({ error: "Something happened when retrieving the books" });
   }
-};
+}
 
-export async function bookByIsbn(req: Request, res: Response){
+export async function bookByIsbn(req: Request, res: Response) {
   const isbn = req.params.isbn;
-  const bookByIsbn = prisma.book.findUnique({
-    where: {isbn: "ezf"}
-  })
 
-  // ((book) => book.isbn === isbn);
-  // if (bookByIsbn) {
-  //   res.json({ bookByIsbn });
-  // } else {
-  //   res.status(404);
-  //   console.log("nothing here");
-  //   res.end();
-  // }
-};
+  try {
+    const book = await prisma.book.findUnique({
+      where: { isbn },
+      include: {
+        reviews: true,
+      },
+    });
+
+    if (book) {
+      res.status(200).json({ bookByIsbn });
+    } else {
+      res.status(404).json({ message: "Nothing was found" });
+      res.end();
+    }
+  } catch (error) {
+    console.error("Error while fetching book by ISBN:", error);
+    res
+      .status(500)
+      .json({ error: "Something happened when retrieving a book by its ISBN" });
+  }
+}
