69f3f87fc00fbe85b4cffca6c5cef6bfd61cfea4
romaneboireau <romane.boireau@abtasty.com>
2025-04-17 15:24:58 +0200

    yarn

---
+++ back-end/dist/books.js
@@ -0,0 +1,71 @@
+"use strict";
+// used to mock the database
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bookList = void 0;
+exports.bookList = [
+    {
+        isbn: "abcd",
+        title: "Theprioryoftheorangetree",
+        author: "Samantha Shannon",
+        reviews: [
+            {
+                reviewId: '1',
+                review: "this is one review.",
+            },
+            {
+                reviewId: '2',
+                review: "second review",
+            },
+        ],
+    },
+    {
+        isbn: "xyz",
+        title: "Theprioryoftheorangetree",
+        author: "Samantha Shannon",
+        reviews: [
+            {
+                reviewId: '3',
+                review: "The Priory of the Orange Tree is a 2019 fantasy novel by writer Samantha Shannon.[1] The novel was published on 26 February 2019 by Bloomsbury Publishing.",
+            },
+            {
+                reviewId: '4',
+                review: "The Priory of the Orange Tree is a 2019 fantasy novel by writer Samantha Shannon.[1] The novel was published on 26 February 2019 by Bloomsbury Publishing.",
+            },
+        ],
+    },
+    {
+        isbn: "978-0-316-54142-8",
+        title: "The Bone Shard Daughter",
+        author: "Andrea Stewart",
+        reviews: [
+            {
+                reviewId: '5',
+                review: "The Bone Shard Daughter is the first book in the Drowning Empire fantasy trilogy by Andrea Stewart, published in 2020.",
+            },
+            {
+                reviewId: '6',
+                review: "The Bone Shard Daughter is the first book in the Drowning Empire fantasy trilogy by Andrea Stewart, published in 2020.",
+            },
+        ],
+    },
+    {
+        isbn: "9780593874813",
+        title: "Lady MacBeth",
+        author: "Ava Reid",
+        reviews: [
+            {
+                reviewId: '6',
+                review: "Reid has said Lady Macbeth is her favorite character, due to her ambition and wiles, which inspired her to explore her character in a novel of her own. She calls the novel a work of 'gothic, feminist fiction'.",
+            }
+        ]
+    },
+    {
+        isbn: "978-0008108427",
+        title: "An Ember in the Ashes",
+        author: "Sabaa Tahir",
+        reviews: [{
+                reviewId: '7',
+                review: "When Laia’s grandparents are brutally murdered and her brother arrested for treason by the empire, the only people she has left to turn to are the rebels.",
+            },]
+    },
+];
---
+++ back-end/dist/routes/bookRoutes.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const bookController_1 = require("../controllers/bookController");
+console.log("📚 bookRoutes loaded");
+const router = express_1.default.Router();
+router.post("/books_list/new_book", bookController_1.newBookMedia);
+router.get("/books_list", bookController_1.allBooks);
+router.get("/books_list/isbn/:isbn", bookController_1.bookByIsbn);
+router.get("/books_list/author/:author", bookController_1.bookByAuthor);
+router.get("/books_list/title/:title", bookController_1.bookByTitle);
+// router.delete("/", deleteReview)
+exports.default = router;
