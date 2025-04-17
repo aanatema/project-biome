51e64ceeb907027e4e2ce10a2d9a9da1a6ce8033
romaneboireau <romane.boireau@abtasty.com>
2025-04-17 15:59:44 +0200

    comment book list example but keep in case of testing

---
+++ front-end/src/Books/BooksLibrary.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import type { Books } from "../../../back-end/src/books.ts";
+export function BooksLibrary() {
+  const [books, setBooks] = useState<Books[]>([]);
+  // by default the books aren't displayed
+  const [showBooks, setShowBooks] = useState(false);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/books/books_list");
+      const json = await response.json();
+      const data: Books[] = json.books;
+      // why are we doing that
+      if (Array.isArray(data)) {
+        setBooks(data);
+        setShowBooks(true);
+      }
+    } catch (error) {
+      console.error("Error, failed to fetch the books", error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <button type="submit" id="get" onClick={fetchBooks}>
+          {" "}
+          GET BOOK{" "}
+        </button>
+        <button type="submit" onClick={() => setShowBooks(false)}>
+          {" "}
+          CLEAR LIST{" "}
+        </button>
+        {/* <button id="get" onClick={fetchBooks}> GET REVIEW</button> */}
+      </div>
+      {showBooks && (
+        <ul>
+          {books.map((book) => (
+            <li key={book.isbn}>
+              {book.title} - {book.author}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
