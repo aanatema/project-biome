479dfe585b47a9b1117d62cafcdd763f153584f7
romaneboireau <romane.boireau@abtasty.com>
2025-04-28 09:23:49 +0200

    change some comments

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -9,7 +9,6 @@ import bookRoutes from "./routes/bookRoutes";
 const app = express();
 const PORT = 3000;
 
-// maybe change this to be more secure ? localhost:3000 doesn't work
 app.use(
   express.json(),
   cors({
@@ -23,11 +22,11 @@ app.use("/books", bookRoutes);
 
 // localhost:3000 home page
 app.get("/", (req: Request, res: Response) => {
-  res.send("this is a get request");
+  res.send("HOME PAGE");
   res.json({ content: "this is some content", url: req.url });
 });
 
-// delete the book
+// delete the book, WIP
 app.delete(
   "/delete/isbn/:isbn/reviews/:reviewId",
   (req: Request, res: Response) => {
@@ -35,6 +34,7 @@ app.delete(
   }
 );
 
+// to verify that the server started
 app.listen(PORT, () => {
   console.log("Server started and listening on port", PORT);
 });
