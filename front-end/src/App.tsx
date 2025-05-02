c0064e85f45416cf66c0468a088b9d39046f4b93
romaneboireau <romane.boireau@abtasty.com>
2025-05-02 17:19:04 +0200

    add routes for pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,21 +1,32 @@
 import "./App.css";
 import { BookForm } from "./MediaForms/BookForm.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound.tsx";
 import { RegisterForm } from "./authForms/RegisterForm.tsx";
-import { LoginForm } from "./authForms/LoginForm.tsx";
-import { BooksLibrary } from "./books/BooksLibrary.tsx";
+import LoginForm from "./authForms/LoginForm.tsx";
+import HomePage from "./pages/homepage.tsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage />, errorElement: <PageNotFound /> },
+  {
+    path: "/register",
+    element: <LoginForm />,
+    errorElement: <PageNotFound />,
+  },
+  {
+    path: "/login",
+    element: <RegisterForm />,
+    errorElement: <PageNotFound />,
+  },
+  {
+    path: "/new-book",
+    element: <BookForm />,
+    errorElement: <PageNotFound />,
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-    create account
-      <RegisterForm />
-      login
-      <LoginForm/>
-      <BookForm />
-      <BooksLibrary/>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
