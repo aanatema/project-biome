e25c48d9eace00cf28c0345979bc1826272cda14
romaneboireau <romane.boireau@abtasty.com>
2025-05-04 16:51:43 +0200

    add type in import

--- front-end/src/components/ui/card.tsx
+++ front-end/src/components/ui/card.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import type * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -32,7 +32,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
+      className={cn("leading-none font-semibold text-center", className)}
       {...props}
     />
   )
