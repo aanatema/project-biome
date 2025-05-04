64a65f58b6ec34823211b022802123f51d50601f
romaneboireau <romane.boireau@abtasty.com>
2025-05-04 15:29:19 +0200

    add react router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { BookForm } from "./MediaForms/BookForm.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound.tsx";
 import { RegisterForm } from "./authForms/RegisterForm.tsx";
 import LoginForm from "./authForms/LoginForm.tsx";
 import HomePage from "./pages/HomePage.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage />, errorElement: <PageNotFound /> },
