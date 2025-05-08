703704f5442e92016c4aeda610930af74847aabf
romaneboireau <romane.boireau@abtasty.com>
2025-05-08 10:43:57 +0200

    create book card WIP

---
+++ front-end/src/components/BookCard.tsx
@@ -0,0 +1,27 @@
+import { Label } from "@radix-ui/react-label";
+import { Card, CardContent, CardFooter } from "./ui/card";
+
+export default function BookCard() {
+  // map of books in database
+// put the cards inside a grid ?
+  return (
+    <Card className="mt-10 w-50 h-80">
+      <CardContent className="space-y-2 text-center">
+        <div className="space-y-1">
+          <Label className="font-bold" htmlFor="title">
+            Book Title
+          </Label>
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="author">Book author</Label>
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="isbn">ISBNr</Label>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Label>Rating</Label>
+      </CardFooter>
+    </Card>
+  );
+}
