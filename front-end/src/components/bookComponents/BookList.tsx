670698beed8b50764be43e7abee9c5fdb3fbe93a
romaneboireau <romane.boireau@abtasty.com>
2025-05-11 10:09:12 +0200

    style

--- front-end/src/components/bookComponents/BookList.tsx
+++ front-end/src/components/bookComponents/BookList.tsx
@@ -25,7 +25,7 @@ export default function BookList() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-10">
       {books.map((book) => (
         <BookCard
           key={book.isbn}
