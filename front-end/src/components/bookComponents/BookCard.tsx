91c7bac1d5b7e80875fa3714d28690abc0b0f4ba
romaneboireau <romane.boireau@abtasty.com>
2025-05-11 10:09:00 +0200

    change in style + add link to the book details page

--- front-end/src/components/bookComponents/BookCard.tsx
+++ front-end/src/components/bookComponents/BookCard.tsx
@@ -1,5 +1,6 @@
 import { Label } from "@radix-ui/react-label";
-import { Card, CardContent, CardFooter } from "../ui/card";
+import { Card, CardContent } from "../ui/card";
+import { Link } from "react-router";
 
 type BookCardProps = {
   title: string;
@@ -11,23 +12,25 @@ export default function BookCard({ title, author, isbn }: BookCardProps) {
   // map of books in database
   // put the cards inside a grid ?
   return (
-    <Card className="mt-10 w-50 h-80">
-      <CardContent className="space-y-2 text-center">
-        <div className="space-y-1">
-          <Label className="font-bold" htmlFor="title">
-            {title}
-          </Label>
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="author">{author}</Label>
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="isbn">{isbn}</Label>
-        </div>
-      </CardContent>
-      {/* <CardFooter>
+    <Link to={`/books/${isbn}`}>
+      <Card className="book-card mt-10 m-5 w-50 h-75">
+        <CardContent className="space-y-2 text-center">
+          <div className="space-y-1">
+            <Label className="font-bold" htmlFor="title">
+              {title}
+            </Label>
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="author">{author}</Label>
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="isbn">{isbn}</Label>
+          </div>
+        </CardContent>
+        {/* <CardFooter>
         <Label>Rating</Label>
       </CardFooter> */}
-    </Card>
+      </Card>
+    </Link>
   );
 }
