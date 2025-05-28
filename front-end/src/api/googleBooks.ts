6deb1e7f71941460e879feab138fa18c2468413b
aanatema <romane.boireau@gmail.com>
2025-05-28 16:55:33 +0200

    add google books api logic

--- front-end/src/api/googleBooks.ts
+++ front-end/src/api/googleBooks.ts
@@ -1,1 +1,30 @@
+import { toast } from "sonner";
 
+export const fetchGoogleBooks = async (isbn: string) => {
+  try {
+    const cleanIsbn = isbn.replace(/[-\s]/g, "");
+
+    const response = await fetch(
+      `http://localhost:3000/books/search-google?q=isbn:${cleanIsbn}`
+    );
+    if (!response.ok) throw new Error("Failed to fetch Google Books data");
+
+    const data = await response.json();
+
+    // Check if items exist in the response
+    const firstBook = data.items?.[0];
+    if (!firstBook) {
+      toast.error("No books found for the given ISBN");
+      return null;
+    }
+
+    const volumeInfo = firstBook.volumeInfo;
+    return {
+      title: volumeInfo.title || "",
+      author: volumeInfo.authors?.[0] || "",
+    };
+  } catch (error) {
+    console.error("Erreur fetchGoogleBooks:", error);
+    return null;
+  }
+};
