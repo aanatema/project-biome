ff36822f4a380d23312b90929be0de8359967589
aanatema <romane.boireau@gmail.com>
2025-05-28 19:09:16 +0200

    change token logic so the user id is taken instead of the user email

--- back-end/dist/auth/auth.tokens.js
+++ back-end/dist/auth/auth.tokens.js
@@ -14,13 +14,13 @@ if (!accessToken)
     throw new Error("access token undefined");
 // generate a jwt based on the user email, can be extended later, see if needed
 const generateAccessToken = (user) => {
-    return (0, jsonwebtoken_1.sign)({ email: user.email }, accessToken, { expiresIn: "30m" });
+    return (0, jsonwebtoken_1.sign)({ id: user.id }, accessToken, { expiresIn: "30m" });
 };
 exports.generateAccessToken = generateAccessToken;
 const refreshToken = process.env.REFRESH_TOKEN_SECRET;
 if (!refreshToken)
     throw new Error("refresh token undefined");
 const generateRefreshToken = (user) => {
-    return (0, jsonwebtoken_1.sign)({ email: user.email }, refreshToken, { expiresIn: "15d" });
+    return (0, jsonwebtoken_1.sign)({ id: user.id }, refreshToken, { expiresIn: "15d" });
 };
 exports.generateRefreshToken = generateRefreshToken;
