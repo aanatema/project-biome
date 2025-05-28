c5ee22459eec05f92dd823a21809a04f36220771
aanatema <romane.boireau@gmail.com>
2025-05-28 19:10:00 +0200

    add verify token to the  login route

--- back-end/src/routes/userRoutes.ts
+++ back-end/src/routes/userRoutes.ts
@@ -1,12 +1,19 @@
 // URL creating a new user
-import express from "express"
-import { createUser, loginUser, modifyUser } from '../controllers/userControllers'
+import express from "express";
+import {
+  createUser,
+  getCurrentUser,
+  loginUser,
+  modifyUser,
+} from "../controllers/userControllers";
+import { verifyToken } from "../auth/auth.middlewares";
 
-const router = express.Router(); 
+const router = express.Router();
 
 router.post("/new_user", createUser);
-// LOGIC TO BE DONE IN THE CONTROLLER FILE
 router.post("/login_user", loginUser);
-router.post("/modify_user", modifyUser);
+// authenticate the user with the token
+router.get("/current_user", verifyToken, getCurrentUser);
+router.post("/modify_user", verifyToken, modifyUser);
 
 export default router;
