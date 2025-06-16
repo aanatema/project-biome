06d51d675d3ec8eab48a93f44b7af6eb1d4a654f
aanatema <romane.boireau@gmail.com>
2025-06-16 14:44:25 +0200

    create terms and conditions page

---
+++ front-end/src/pages/TermsAndConditionsPage.tsx
@@ -0,0 +1,11 @@
+import NavigationMenuDemo from "@/components/websiteComponents/Navbar";
+import { TermsConditions } from "@/components/websiteComponents/TermsConditions";
+
+export default function TermsAndConditionsPage() {
+  return (
+    <>
+      <NavigationMenuDemo />
+      <TermsConditions/>
+    </>
+  );
+}
