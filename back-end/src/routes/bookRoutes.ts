7fe605de76b8f50fd98749db69995c60c8a63df3
aanatema <romane.boireau@gmail.com>
2025-06-16 09:51:56 +0200

    comment unused / unsuable path for now

--- back-end/src/routes/bookRoutes.ts
+++ back-end/src/routes/bookRoutes.ts
@@ -1,24 +1,28 @@
 import express from "express";
 import {
-  newBookMedia,
-  allBooks,
-  bookByAuthor,
-  bookByIsbn,
-  bookByTitle,
-  searchGoogleBooks,
+  createBook,
+  createReview,
+  getUserReviews,
+  // bookByAuthor,
+  // bookByIsbn,
+  // bookByTitle,
+  // searchGoogleBooks,
 } from "../controllers/bookController";
 
 console.log("📚 bookRoutes loaded");
 
 const router = express.Router();
 
-router.post("/new_book", newBookMedia);
-router.get("/books", allBooks);
-router.get("/books/isbn/:isbn", bookByIsbn);
-router.get("/search-google", searchGoogleBooks);
-router.get("/books/author/:author", bookByAuthor);
-router.get("/books/title/:title", bookByTitle);
-router.get("/books/isbn/:title", bookByTitle);
+router.post("/new_book", createBook);
+router.post("/new_review", createReview);
+router.post("/user_reviews", getUserReviews);
+// router.get("/books", allBooks);
+
+// router.get("/books/isbn/:isbn", bookByIsbn);
+// router.get("/search-google", searchGoogleBooks);
+// router.get("/books/author/:author", bookByAuthor);
+// router.get("/books/title/:title", bookByTitle);
+// router.get("/books/isbn/:title", bookByTitle);
 
 // router.delete("/", deleteReview)
 
