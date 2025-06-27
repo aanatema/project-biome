74525d869bc6dddb22ffda6d450e18e09f567084
aanatema <romane.boireau@gmail.com>
2025-06-27 10:15:42 +0200

    change in path name

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'sonner'
-import { AuthProvider } from './context/AuthContext.tsx'
+import { AuthProvider } from "./context/AuthProvider.tsx";
 
 
 createRoot(document.getElementById("root")!).render(
