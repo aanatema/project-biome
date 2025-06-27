69ae11500b5a3d5886ee050e43162fb5b57db20e
aanatema <romane.boireau@gmail.com>
2025-06-27 11:10:40 +0200

    remove unused function

--- back-end/src/auth/auth.tokens.ts
+++ back-end/src/auth/auth.tokens.ts
@@ -1,17 +1,11 @@
 import { sign } from "jsonwebtoken";
 import type { JwtPayload } from "./auth.types";
 
-export function generateTokens(user: JwtPayload) {
-  return {
-    accessToken: generateAccessToken(user),
-    refreshToken: generateRefreshToken(user),
-  };
-}
 
 const accessToken = process.env.ACCESS_TOKEN_SECRET;
 if (!accessToken) throw new Error("access token undefined");
 
-// generate a jwt based on the user email, can be extended later, see if needed
+// generate a jwt based on the user id, can be extended later, see if needed
 export const generateAccessToken = (user: JwtPayload): string => {
   return sign({ id: user.id }, accessToken, { expiresIn: "30m" });
 };
