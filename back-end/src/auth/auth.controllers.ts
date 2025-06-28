66d3f7604d9c701cf881c5f5e47d976340807ea5
aanatema <romane.boireau@gmail.com>
2025-06-28 16:10:43 +0200

    style

--- back-end/src/auth/auth.controllers.ts
+++ back-end/src/auth/auth.controllers.ts
@@ -4,16 +4,16 @@ import { verify } from "jsonwebtoken";
 import prisma from "../lib/prisma";
 
 export async function refreshAccessToken(req: Request, res: Response) {
-  const refreshToken = process.env.REFRESH_TOKEN_SECRET;
-  if (!refreshToken)
-    throw new Error("refresh token undefined in refreshAccessToken func");
+	const refreshToken = process.env.REFRESH_TOKEN_SECRET;
+	if (!refreshToken)
+		throw new Error("refresh token undefined in refreshAccessToken func");
 
-  const token = req.cookies.refreshToken;
-  if (!token) {
+	const token = req.cookies.refreshToken;
+	if (!token) {
 		return res.status(401).json({ error: "No refresh token provided" });
-  }
-  
-  try {
+	}
+
+	try {
 		const payload = verify(token, refreshToken) as { id: string };
 		const user = await prisma.user.findUnique({
 			where: {
@@ -25,9 +25,12 @@ export async function refreshAccessToken(req: Request, res: Response) {
 		// remove password from user object
 		const { password: _password, ...userWithoutPassword } = user;
 		const newAccessToken = generateAccessToken(userWithoutPassword);
+
 		return res.status(200).json({ token: newAccessToken });
-  } catch (err) {
-    console.error("Invalid refresh token", err);
-    return res.status(401).json({ error: "Invalid or expired refresh token" });
-  }
+	} catch (err) {
+		console.error("Invalid refresh token", err);
+		return res
+			.status(401)
+			.json({ error: "Invalid or expired refresh token" });
+	}
 }
