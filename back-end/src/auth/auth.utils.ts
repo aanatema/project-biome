0df893934a4288eff85a2d29355ce627dc358bf9
aanatema <romane.boireau@gmail.com>
2025-06-28 14:37:06 +0200

    delete unused file

--- back-end/src/auth/auth.utils.ts
+++
@@ -1,6 +0,0 @@
-import type { User } from "@prisma/client";
-
-export function sanitizeUser(user: User){
-  const { password: _password, ...userWithoutPassword } = user;
-  return userWithoutPassword;
-}
