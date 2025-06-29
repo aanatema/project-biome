8adc6256a224afe35e63e894c46586932996793f
aanatema <romane.boireau@gmail.com>
2025-06-29 20:05:04 +0200

    change in the way booklist is created to match reviews requirement

--- front-end/src/components/bookComponents/BookList.tsx
+++ front-end/src/components/bookComponents/BookList.tsx
@@ -2,38 +2,40 @@ import { useEffect, useState } from "react";
 import BookCard from "./BookCard";
 
 type Book = {
-  isbn: string;
-  title: string;
-  author: string;
+	id: string;
+	isbn: string;
+	title: string;
+	author: string;
 };
 
 export default function BookList() {
-  const [books, setBooks] = useState<Book[]>([]);
+	const [books, setBooks] = useState<Book[]>([]);
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const res = await fetch("http://localhost:3000/books/books");
-      if (!res.ok) {
-        console.error("Erreur API");
-        return;
-      }
-      const json = await res.json();
-      setBooks(json);
-    };
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const res = await fetch("http://localhost:3000/books/books");
+			if (!res.ok) {
+				console.error("Erreur API");
+				return;
+			}
+			const json = await res.json();
+			setBooks(json);
+		};
 
-    fetchBooks();
-  }, []);
+		fetchBooks();
+	}, []);
 
-  return (
-		<div className='grid grid-cols-1 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 ml-5 mr-5 mt-10'>
+	return (
+		<div className='grid grid-cols-1 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-6 xl:grid-cols-6 ml-5 mr-5 mt-10'>
 			{books.map((book) => (
 				<BookCard
-					key={book.isbn}
+					key={book.id}
+					id={book.id}
 					title={book.title}
 					author={book.author}
 					isbn={book.isbn}
 				/>
 			))}
 		</div>
-  );
+	);
 }
