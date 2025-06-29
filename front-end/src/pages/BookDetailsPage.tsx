aae31204826ef86cf251634e7aea99e685ef4af9
aanatema <romane.boireau@gmail.com>
2025-06-29 13:45:53 +0200

    formating and minor changes

--- front-end/src/pages/BookDetailsPage.tsx
+++ front-end/src/pages/BookDetailsPage.tsx
@@ -1,43 +1,48 @@
+import BookCard from "@/components/bookComponents/BookCard";
 import ReviewCard from "@/components/bookComponents/ReviewCard";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 type Book = {
-  isbn: string;
-  title: string;
-  author: string;
-  reviews?: { review: string }[];
+	isbn: string;
+	title: string;
+	author: string;
+	reviews?: { review: string }[];
 };
 
 export default function BookDetailsPage() {
-  const { isbn } = useParams<{ isbn: string }>();
-  const [book, setBook] = useState<Book | null>(null);
-  const [error, setError] = useState("");
+	const { isbn } = useParams<{ isbn: string }>();
+	const [book, setBook] = useState<Book | null>(null);
+	const [error, setError] = useState("");
 
-  useEffect(() => {
-    if (!isbn) {
-      setError("Aucun ISBN n’a été fourni dans l’URL");
-      return;
-    }
-    
-    const fetchBook = async () => {
-      try {
-        const res = await fetch(`http://localhost:3000/books/isbn/${isbn}`);
-        if (!res.ok) throw new Error("Book not found");
-        const json = await res.json();
-        setBook(json.book);
-      } catch (err) {
-        setError("Failed to load book details");
-      }
-    };
+	useEffect(() => {
+		const fetchBook = async () => {
+			try {
+				const res = await fetch(
+					`http://localhost:3000/books/isbn/${isbn}`
+				);
+				if (!res.ok) throw new Error("Book not found");
+				const json = await res.json();
+				setBook(json.book);
+			} catch (err) {
+				setError("Failed to load book details");
+			}
+		};
 
-    fetchBook();
-  }, [isbn]);
+		fetchBook();
+	}, [isbn]);
 
-  if (error) return <p className="text-center mt-10 text-red-600">{error}</p>;
-  if (!book) return <p className="text-center mt-10">Loading...</p>;
+	if (error) return <p className='text-center mt-10 text-red-600'>{error}</p>;
+	if (!book) return <p className='text-center mt-10'>Loading...</p>;
 
-  return (
-   <ReviewCard/>
-  );
+	return (
+		<>
+			<BookCard
+				title={""}
+				author={""}
+				isbn={""}
+			/>
+			<ReviewCard />
+		</>
+	);
 }
