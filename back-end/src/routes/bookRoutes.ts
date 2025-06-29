733cce6ed1160207972a2670d52b63854939bd66
aanatema <romane.boireau@gmail.com>
2025-06-29 13:46:42 +0200

    remove unused elements

--- back-end/src/routes/bookRoutes.ts
+++ back-end/src/routes/bookRoutes.ts
@@ -1,15 +1,9 @@
 import express from "express";
 import {
 	allBooks,
-	bookByIsbn,
-	createBook,
 	createBookAndReview,
-	createReview,
+	getReviewsByBookId,
 	getUserReviews,
-	// bookByAuthor,
-	// bookByIsbn,
-	// bookByTitle,
-	// searchGoogleBooks,
 } from "../controllers/bookController";
 import { verifyToken } from "../auth/auth.middlewares";
 
@@ -20,15 +14,8 @@ const router = express.Router();
 // router.post("/new_book", createBook);
 // router.post("/new_review", createReview);
 router.post("/add_book_and_review", verifyToken, createBookAndReview);
-router.post("/user_reviews",verifyToken, getUserReviews);
+router.get("/books/:id/reviews", getReviewsByBookId);
+router.post("/user_reviews", verifyToken, getUserReviews);
 router.get("/books", allBooks);
-
-router.get("/books/isbn/:isbn", bookByIsbn);
-// router.get("/search-google", searchGoogleBooks);
-// router.get("/books/author/:author", bookByAuthor);
-// router.get("/books/title/:title", bookByTitle);
-// router.get("/books/isbn/:title", bookByTitle);
-
-// router.delete("/", deleteReview)
 
 export default router;
