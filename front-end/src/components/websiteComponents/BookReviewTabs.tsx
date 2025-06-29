97acea01a78f434ecee61d8554418ae5202dcb88
aanatema <romane.boireau@gmail.com>
2025-06-29 13:20:10 +0200

    create bookreview tabs to simplify home page file

---
+++ front-end/src/components/websiteComponents/BookReviewTabs.tsx
@@ -0,0 +1,37 @@
+import BookList from "../bookComponents/BookList";
+import ReviewCard from "../bookComponents/ReviewCard";
+import {
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from "../shadcnComponents/tabs";
+
+export default function BookReviewTabs() {
+	return (
+		<Tabs
+			defaultValue='reviews'
+			className='mt-10 w-150'>
+			<TabsList className='grid w-full grid-cols-2'>
+				<TabsTrigger
+					className='rounded-l-md'
+					value='reviews'>
+					Reviews
+				</TabsTrigger>
+				<TabsTrigger
+					className='rounded-r-md'
+					value='books'>
+					Books
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent
+				className='mt-10'
+				value='reviews'>
+				<ReviewCard />
+			</TabsContent>
+			<TabsContent value='books'>
+				<BookList />
+			</TabsContent>
+		</Tabs>
+	);
+}
