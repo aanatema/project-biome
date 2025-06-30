5bb8657d78e17593ae2058a553d402e6029f29c4
aanatema <romane.boireau@gmail.com>
2025-06-30 10:04:19 +0200

    add book info to review card

--- front-end/src/components/bookComponents/ReviewCard.tsx
+++ front-end/src/components/bookComponents/ReviewCard.tsx
@@ -9,28 +9,47 @@ type ReviewCardProps = {
 		id: string;
 		username: string;
 	};
+	book: string;
+	bookAuthor: string;
 };
 
-export default function ReviewCard({ content, author }: ReviewCardProps) {
+export default function ReviewCard({
+	content,
+	author,
+	book,
+	bookAuthor,
+}: ReviewCardProps) {
 	return (
 		<>
-			<Card className='book-card mb-5 min-w-70 max-w-150 min-h-30 max-h-60 overflow-scroll'>
+			<Card className='book-card mb-5 min-w-70 max-w-150 min-h-30 max-h-60 overflow-y-scroll'>
 				<CardContent className='space-y-2 text-center'>
-					<Label
-						className='font-bold'
-						htmlFor='username'>
-						hey
-					</Label>
-					<Label
-						className='font-bold'
-						htmlFor='username'>
-						{author.username}
-					</Label>
-					<Label
-						className='pb-4'
-						htmlFor='review'>
-						{content}
-					</Label>
+					<div className='text-start'>
+						<Label
+							className='font-bold'
+							htmlFor='username'>
+							{author.username}
+						</Label>
+
+						<div>
+							<Label
+								className='font-medium text-sm'
+								htmlFor='book'>
+								{book} |{" "}
+							</Label>
+							<Label
+								className='text-sm italic'
+								htmlFor='book'>
+								{bookAuthor}
+							</Label>
+						</div>
+					</div>
+					<div className='text-start'>
+						<Label
+							className='pb-4 text-start'
+							htmlFor='review'>
+							{content}
+						</Label>
+					</div>
 				</CardContent>
 			</Card>
 		</>
