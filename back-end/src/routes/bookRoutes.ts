fd9c20a1fa2daf8702070defc87e26d6e306dfe8
aanatema <romane.boireau@gmail.com>
2025-06-30 23:58:21 +0200

    logic of getting users book

--- back-end/src/routes/bookRoutes.ts
+++ back-end/src/routes/bookRoutes.ts
@@ -5,7 +5,7 @@ import {
 	getAllReviews,
 	getBookByIsbn,
 	getReviewsByBookId,
-	getUserReviews,
+	getUserBooks,
 } from "../controllers/bookController";
 import { verifyToken } from "../auth/auth.middlewares";
 
@@ -13,10 +13,8 @@ console.log("📚 bookRoutes loaded");
 
 const router = express.Router();
 
-// router.post("/new_book", createBook);
-// router.post("/new_review", createReview);
 router.post("/add_book_and_review", verifyToken, createBookAndReview);
-router.post("/user_reviews", verifyToken, getUserReviews);
+router.get("/user_books", verifyToken, getUserBooks);
 
 router.get("/books", allBooks);
 router.get("/reviews", getAllReviews);
