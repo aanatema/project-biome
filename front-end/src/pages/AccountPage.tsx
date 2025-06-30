93473b31bb1314b88a062c8e52418bc39a2a8c78
aanatema <romane.boireau@gmail.com>
2025-06-30 23:58:46 +0200

    use userbooks component

--- front-end/src/pages/AccountPage.tsx
+++ front-end/src/pages/AccountPage.tsx
@@ -1,8 +1,8 @@
 import Navbar from "@/components/websiteComponents/Navbar";
 import { useAuth } from "@/Hooks/useAuth";
 import NotConnectedPage from "./NotConnectedPage";
-import BookList from "@/components/bookComponents/BookList";
 import { ProfileSettingsButton } from "@/components/userComponents/ProfileSettings";
+import UserBooks from "@/components/bookComponents/UserBooks";
 
 // list of all books linked to this account
 export default function AccountPage() {
@@ -21,8 +21,8 @@ export default function AccountPage() {
 					<h1 className='ml-10 font-semibold text-lg '>Your books</h1>
 				</div>
 
-				<div className='w-full flex justify-center'>
-					<BookList />
+				<div className='w-full flex justify-start pl-10 '>
+					<UserBooks />
 				</div>
 			</div>
 		</>
