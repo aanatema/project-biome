29524464ff94b65838ca7735db05b11daf032d53
aanatema <romane.boireau@gmail.com>
2025-07-01 00:19:38 +0200

    add 'add book' button to profile to make things smoother

--- front-end/src/pages/AccountPage.tsx
+++ front-end/src/pages/AccountPage.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "@/Hooks/useAuth";
 import NotConnectedPage from "./NotConnectedPage";
 import { ProfileSettingsButton } from "@/components/userComponents/ProfileSettings";
 import UserBooks from "@/components/bookComponents/UserBooks";
+import { Button } from "@/components/shadcnComponents/button";
 
 // list of all books linked to this account
 export default function AccountPage() {
@@ -14,7 +15,10 @@ export default function AccountPage() {
 		<>
 			<Navbar />
 			<div className='flex flex-col items-center p-4'>
-				<div className='w-full flex justify-end mb-4 mr-10 mt-5'>
+				<div className='w-full flex justify-end mb-4 mr-10 mt-5 gap-5'>
+					<Button variant='secondary'>
+						<a href='/new-book'>Add a book</a>
+					</Button>
 					<ProfileSettingsButton />
 				</div>
 				<div className='w-full flex justify-start mt-5'>
