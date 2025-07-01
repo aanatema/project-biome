818e2226760f5bac2eab9f50eb07a8a685f9c950
aanatema <romane.boireau@gmail.com>
2025-07-02 01:02:44 +0200

    formatting

--- back-end/src/routes/userRoutes.ts
+++ back-end/src/routes/userRoutes.ts
@@ -1,4 +1,3 @@
-// URL creating a new user
 import express from "express";
 import {
 	createUser,
@@ -15,9 +14,9 @@ const router = express.Router();
 router.post("/new_user", createUser);
 router.post("/login_user", loginUser);
 router.post("/logout_user", logoutUser);
-// authenticate the user with the token
-router.get("/current_user", verifyToken, getCurrentUser);
 router.post("/modify_user", verifyToken, modifyUser);
+
+router.get("/current_user", verifyToken, getCurrentUser);
 
 router.delete("/delete_user", verifyToken, deleteUser);
 export default router;
