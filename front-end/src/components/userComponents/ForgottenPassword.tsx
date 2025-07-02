f46cc5ecbc92b4353fa3968453e4bb24803a1264
aanatema <romane.boireau@gmail.com>
2025-07-02 13:30:02 +0200

    uncomment logic to avoid fail wip

--- front-end/src/components/userComponents/ForgottenPassword.tsx
+++ front-end/src/components/userComponents/ForgottenPassword.tsx
@@ -14,43 +14,44 @@ import { DialogClose } from "@radix-ui/react-dialog";
 import { useContext } from "react";
 import { toast } from "sonner";
 import { Input } from "../shadcnComponents/input";
-import { Label } from "@radix-ui/react-label";
 
 export default function ForgottenPasswordDialog() {
-	// const auth = useContext(AuthContext);
+	const auth = useContext(AuthContext);
 
-	// const sendEmail = async () => {
-	// 	if (!auth) return;
+	const sendEmail = async () => {
+		if (!auth) return;
 
-	// 	try {
-	// 		const refreshRes = await authApi.post("/refresh");
-	// 		const accessToken = refreshRes.data.token;
+		try {
+			const refreshRes = await authApi.post("/refresh");
+			const accessToken = refreshRes.data.token;
 
-	// 		const res = await fetch("http://localhost:3000/users/delete_user", {
-	// 			method: "DELETE",
-	// 			headers: {
-	// 				Authorization: `Bearer ${accessToken}`,
-	// 			},
-	// 		});
+			const res = await fetch("http://localhost:3000/users/delete_user", {
+				method: "DELETE",
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			});
 
-	// 		if (!res.ok) throw new Error("Failed to delete account");
-	// 		toast.success("Mail sent, check your inbox!");
-	// 	} catch (err) {
-	// 		console.error("Error while looking for account", err);
-	// 		toast.error("We couldn't find your account, try again later or create one");
-	// 	}
-	// };
+			if (!res.ok) throw new Error("Failed to delete account");
+			toast.success("Mail sent, check your inbox!");
+		} catch (err) {
+			console.error("Error while looking for account", err);
+			toast.error(
+				"We couldn't find your account, try again later or create one"
+			);
+		}
+	};
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
-				<p className=' text-decoration-line: underline'>
+				<p className=' text-decoration-line: underline text-secondary'>
 					Forgot your password?
 				</p>
 			</DialogTrigger>
 			<DialogContent className='sm:max-w-[425px]'>
 				<DialogHeader>
 					<DialogTitle>Forgot your password?</DialogTitle>
-					<DialogDescription className="mb-5">
+					<DialogDescription>
 						Enter your email and we'll check if you already have an
 						account, if that's the case, we'll send you an email
 						allowing you to reset your password.
@@ -66,7 +67,9 @@ export default function ForgottenPasswordDialog() {
 							Confirm
 						</Button>
 						<DialogClose
-							className={buttonVariants({ variant: "destructive" })}>
+							className={buttonVariants({
+								variant: "destructive",
+							})}>
 							Cancel
 						</DialogClose>
 					</div>
