4aeb12f99a0bb00fb0a760b91c68451cf951b510
aanatema <romane.boireau@gmail.com>
2025-07-04 11:53:15 +0200

    add some folder for better clarity

--- back-end/src/routes/bookRoutes.ts
+++ back-end/src/routes/bookRoutes.ts
@@ -15,14 +15,14 @@ console.log("📚 bookRoutes loaded");
 
 const router = express.Router();
 
-router.post("/add_book_and_review", verifyToken, createBookAndReview);
+// router.post("/add_book_and_review", verifyToken, createBookAndReview);
 
-router.get("/books", allBooks);
-router.get("/reviews", getAllReviews);
-router.get("/isbn/:isbn", getBookByIsbn);
-router.get("/:bookId/reviews", getReviewsByBookId);
-router.get("/user_books", verifyToken, getUserBooks);
+// router.get("/books", allBooks);
+// router.get("/reviews", getAllReviews);
+// router.get("/isbn/:isbn", getBookByIsbn);
+// router.get("/:bookId/reviews", getReviewsByBookId);
+// router.get("/user_books", verifyToken, getUserBooks);
 
-router.delete("/reviews/:id", verifyToken, deleteReview);
+// router.delete("/reviews/:id", verifyToken, deleteReview);
 
 export default router;
