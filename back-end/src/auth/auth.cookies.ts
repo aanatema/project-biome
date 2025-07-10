08c35931e9e92097d4e72ed2370164d12b77a558
aanatema <romane.boireau@gmail.com>
2025-07-10 10:04:58 +0200

    remove access token

--- back-end/src/auth/auth.cookies.ts
+++ back-end/src/auth/auth.cookies.ts
@@ -9,13 +9,3 @@ const refreshTokenOptions = {
 export function setRefreshTokenCookie(res: Response, token: string) {
 	res.cookie("refreshToken", token, refreshTokenOptions);
 }
-
-const accessTokenOptions = {
-	httpOnly: true,
-	secure: false, //false for dev, true for prod
-	sameSite: "lax" as const,
-	maxAge: 1000 * 60 * 15, // 15 min
-};
-export function setAccessTokenCookie(res: Response, token: string) {
-	res.cookie("accessToken", token, accessTokenOptions);
-}
