2b9a003ec26306944b6834bc443ac7d5475c1682
aanatema <romane.boireau@gmail.com>
2025-09-29 19:30:23 +0200

    fix yellow color variable

--- front-end/src/components/shadcnComponents/textarea.tsx
+++ front-end/src/components/shadcnComponents/textarea.tsx
@@ -4,15 +4,15 @@ import { cn } from "@/libraries/tailwind";
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
-    <textarea
-      data-slot="textarea"
-      className={cn(
-        "border-input bg-background placeholder:text-muted-foreground focus-visible:border-secondary-foreground focus-visible:ring-secondary-foreground aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-[100px] w-full rounded-md border px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[2px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
-      )}
-      {...props}
-    />
-  )
+		<textarea
+			data-slot='textarea'
+			className={cn(
+				"border-input bg-background placeholder:text-muted-foreground focus-visible:border-yellow focus-visible:ring-yellow aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-[100px] w-full rounded-md border px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[2px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+				className
+			)}
+			{...props}
+		/>
+  );
 }
 
 export { Textarea }
