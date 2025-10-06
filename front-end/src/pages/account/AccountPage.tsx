9a1614992718ed756dde2844ebfdcc69360dd578
aanatema <romane.boireau@gmail.com>
2025-10-06 10:40:01 +0200

    create buttons folder, create addbookbutton

--- front-end/src/pages/account/AccountPage.tsx
+++ front-end/src/pages/account/AccountPage.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/websiteComponents/Navbar";
 import { useAuth } from "@/hooks/useAuth";
 import UserBooks from "@/components/bookComponents/UserBooks";
-import { Button } from "@/components/shadcnComponents/button";
 import { ProfileButton } from "@/components/userComponents/ProfileSettings";
 import NotConnectedPage from "../connection/NotConnectedPage";
+import AddBookButton from "@/components/buttons/AddBookButton";
 
 // list of all books linked to this account
 export default function AccountPage() {
@@ -16,10 +16,8 @@ export default function AccountPage() {
 			<Navbar />
 			<div className='flex flex-col items-center p-4'>
 				<div className='w-full flex justify-end mb-4 mr-10 mt-5 gap-5'>
-					<Button variant='secondary'>
-						<a href='/new-book'>Add a book</a>
-					</Button>
-					<ProfileButton/>
+					<AddBookButton />
+					<ProfileButton />
 				</div>
 				<div className='w-full flex justify-start mt-5'>
 					<h1 className='ml-10 font-semibold text-lg '>Your books</h1>
