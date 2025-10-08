de17c895ae8a7f07b4b884362f0cfecd095a3a1f
aanatema <romane.boireau@gmail.com>
2025-10-08 10:57:45 +0200

    change in display of books card

--- front-end/src/components/bookComponents/BookReviews.tsx
+++ front-end/src/components/bookComponents/BookReviews.tsx
@@ -84,7 +84,7 @@ export default function BookReviews() {
 				</div>
 			) : (
 				<>
-					<div className='grid grid-cols-2 gap-4'>
+					<div className='grid grid-cols-1 md:grid-cols-2 gap-4 '>
 						{reviews.map((review) => (
 							<BookDetailsReviewCard
 								key={review.id}
