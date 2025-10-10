ebe2a85fe3f04ae3f72d416881d47479d90dc90e
aanatema <romane.boireau@gmail.com>
2025-10-10 10:04:31 +0200

    changes in path and places

--- front-end/src/pages/connection/LogOrRegisterPage.tsx
+++ front-end/src/pages/connection/LogOrRegisterPage.tsx
@@ -1,13 +1,13 @@
-import LogRegisterForm from "@/components/userComponents/LogRegisterForm";
+import LogRegisterForm from "@/pages/connection/components/LogRegisterForm";
 import Navbar from "@/components/websiteComponents/Navbar";
 
 export default function LogOrRegisterPage() {
-  return (
+	return (
 		<>
 			<Navbar />
 			<div className='flex flex-col items-center justify-center'>
 				<LogRegisterForm />
 			</div>
 		</>
-  );
+	);
 }
