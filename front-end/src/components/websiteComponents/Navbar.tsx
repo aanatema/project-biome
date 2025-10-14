235c7f9aae57e8484610baa55245e4e9e8ac726a
aanatema <romane.boireau@gmail.com>
2025-10-14 19:08:13 +0200

    remove hydration error on navbar (NavigationMenuLink asChild with next Link inside instead of outside)

--- front-end/src/components/websiteComponents/Navbar.tsx
+++ front-end/src/components/websiteComponents/Navbar.tsx
@@ -1,8 +1,3 @@
-"use client";
-
-import * as React from "react";
-
-import { cn } from "../../libraries/tailwind";
 import {
 	NavigationMenu,
 	NavigationMenuItem,
@@ -17,57 +12,27 @@ export default function Navbar() {
 		<NavigationMenu className='sticky top-0 pt-2 pb-2 shadow-sm bg-background'>
 			<NavigationMenuList>
 				<NavigationMenuItem>
-					<Link to='/'>
-						<NavigationMenuLink
-							className={navigationMenuTriggerStyle()}>
-							Homepage
-						</NavigationMenuLink>
-					</Link>
+					<NavigationMenuLink
+						asChild
+						className={navigationMenuTriggerStyle()}>
+						<Link to='/'>Homepage</Link>
+					</NavigationMenuLink>
 				</NavigationMenuItem>
 				<NavigationMenuItem>
-					<Link to='/new-book'>
-						<NavigationMenuLink
-							className={navigationMenuTriggerStyle()}>
-							Add a book
-						</NavigationMenuLink>
-					</Link>
+					<NavigationMenuLink
+						asChild
+						className={navigationMenuTriggerStyle()}>
+						<Link to='/new-book'>Add a book</Link>
+					</NavigationMenuLink>
 				</NavigationMenuItem>
 				<NavigationMenuItem>
-					<Link to='/account'>
-						<NavigationMenuLink
-							className={navigationMenuTriggerStyle()}>
-							Account
-						</NavigationMenuLink>
-					</Link>
+					<NavigationMenuLink
+						asChild
+						className={navigationMenuTriggerStyle()}>
+						<Link to='/account'>Account</Link>
+					</NavigationMenuLink>
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
 	);
 }
-
-const ListItem = React.forwardRef<
-	React.ElementRef<"a">,
-	React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-	return (
-		<li>
-			<NavigationMenuLink asChild>
-				<a
-					ref={ref}
-					className={cn(
-						"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-						className
-					)}
-					{...props}>
-					<div className='text-sm font-medium leading-none'>
-						{title}
-					</div>
-					<p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
-						{children}
-					</p>
-				</a>
-			</NavigationMenuLink>
-		</li>
-	);
-});
-ListItem.displayName = "ListItem";
